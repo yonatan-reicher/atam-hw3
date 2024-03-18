a302ee8ad4ba4e0d81a594c9c62e3328d3b3f1fb
Yonatan Reicher <yony252525@gmail.com>
2024-03-18 07:22:01 +0000

    First working draft of code!

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -3,5 +3,63 @@
 .text
 .align 4, 0x90
 my_ili_handler:
-  ####### Some smart student's code here #######
+  # The address of the invalid instruction is the first pointer 8 bytes
+  # on the stack.
+  push %rbp
+  mov %rsp, %rbp
+
+  push %rax
+  push %rbx
+  push %rdx
+  push %rdi
+ 
+  xor %rax, %rax
+  xor %rbx, %rbx
+  xor %rdx, %rdx
+
+  # RAX - Address of instruction
+  # RDX - opcode holder
+  # RBX, RCX - helpers
+
+  # Set rax. The address is on the stack (from the frame).
+  mov 8(%rbp), %rax
+
+  # I think the opcode starts at the lower byte?
+  mov 0(%rax), %dl
+  
+  # Testing first byte is 0F
+  cmp $0x0F, %dl
+  jne length_is_1
+  # Length is 2. Skip the first one.
+  mov 1(%rax), %dl
+length_is_1:
+
+  # Call the function!
+  mov %rdx, %rdi
+  mov %rax, %rbx
+  call what_to_do
+  cmp $0, %rax
+  jnz my_return_path
+  # Do the original interrupt on a rax=0!
+  pop %rdi
+  pop %rdx
+  pop %rbx
+  pop %rax
+  pop %rbp
+  # This code will call iretq
+  jmp *old_ili_handler
+my_return_path:
+  # What we need to do:
+  # 1. RDI <- RAX
+  # 2. Set return address to one instruction after!
+  mov %rax, %rdi
+  # Right now RBX has the return address
+  inc %rbx
+  mov %rbx, 8(%rbp)
+
+  pop %rdx # Ignore the stored %rdi value!
+  pop %rdx
+  pop %rbx
+  pop %rax
+  pop %rbp
   iretq
--- part2/ili_utils.c
+++ part2/ili_utils.c
@@ -1,25 +1,31 @@
 #include <asm/desc.h>
 
 void my_store_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
-// </STUDENT FILL>
+	asm volatile (
+		"sidt %0"
+		:
+		"=m"(*idtr)
+	);
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
-// <STUDENT FILL>
+	asm volatile (
+		"lidt %0"
+		::
+		"m"(*idtr)
+	);
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
-// </STUDENT FILL>
+	gate->offset_low = addr;
+	gate->offset_middle = addr >> 16;
+	gate->offset_high = addr >> 32;
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
-// </STUDENT FILL>
+	unsigned long ret = 0;
+	ret |= gate->offset_low;
+	ret |= (unsigned long)gate->offset_middle << 16;
+	ret |= (unsigned long)gate->offset_high << 32;
+	return ret;
 }
